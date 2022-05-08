5db45f51b5dc4e5b6ac4558f633221c31da4095b
cuitlazotlac <cuitlazotlac@gmail.com>
2022-05-08 00:54:46 -0400

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { ethers } from "ethers";
+
+import DecentratwitterAbi from "./contractsData/decentratwitter.json";
+import DecentratwitterAddress from "./contractsData/decentratwitter-address.json";
+
+import {
+  Spinner,
+  Navbar,
+  Nav,
+  Button,
+  Container,
+  Image,
+} from "react-bootstrap";
+
+import logo from "./assets/toast.png";
+import missing_wallet from "./assets/missing_wallet.png";
+import Home from "./Home.jsx";
+import Profile from "./Profile.jsx";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [account, setAccount] = useState(null);
+  const [contract, setContract] = useState({});
+
+  const web3Handler = async () => {
+    let accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+
+    // Setup event listeners for metamask
+    window.ethereum.on("chainChanged", () => {
+      window.location.reload();
+    });
+    window.ethereum.on("accountsChanged", async () => {
+      setLoading(true);
+      web3Handler();
+    });
+    // Get provider from Metamask
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // Get signer
+    const signer = provider.getSigner();
+    loadContract(signer);
+  };
+  const loadContract = async (signer) => {
+    // Get deployed copy of Decentratwitter contract
+    const contract = new ethers.Contract(
+      DecentratwitterAddress.address,
+      DecentratwitterAbi.abi,
+      signer
+    );
+    setContract(contract);
+    setLoading(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          First commit !
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <>
+          <Navbar expand="lg" bg="dark" variant="dark" className="main-title">
+            <Container>
+              <Navbar.Brand href="">
+                <img src={logo} width="40" height="40" alt="" />
+                <h6 className="title">Spread</h6>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/profile">
+                    Profile
+                  </Nav.Link>
+                </Nav>
+                <Nav>
+                  {account ? (
+                    <Nav.Link
+                      href={`https://etherscan.io/address/${account}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="button nav-button btn-sm mx-4"
+                    >
+                      <Button variant="outline-light">
+                        {account.slice(0, 5) + "..." + account.slice(38, 42)}
+                      </Button>
+                    </Nav.Link>
+                  ) : (
+                    <Button onClick={web3Handler} variant="outline-light">
+                      Connect Wallet
+                    </Button>
+                  )}
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </>
+        <div>
+          {loading ? (
+            <div
+              style={
+                {
+                  // display: "flex",
+                  // justifyContent: "center",
+                  // alignItems: "center",
+                  // minHeight: "80vh",
+                }
+              }
+            >
+              {/* <Spinner animation="border" style={{ display: "flex" }} /> */}
+              <div>
+                <Image className="image_focus" src={missing_wallet} />
+              </div>
+              <p className="mx-3 my-0">
+                You need to connect a wallet to use this application
+              </p>
+              <div>
+                <br />
+                <Button onClick={web3Handler} variant="outline-dark">
+                  Connect Wallet
+                </Button>
+              </div>
+            </div>
+          ) : (
+            <Routes>
+              <Route path="/" element={<Home contract={contract} />} />
+              <Route
+                path="/profile"
+                element={<Profile contract={contract} />}
+              />
+            </Routes>
+          )}
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
+
+<style type="text/css">
+  {`
+  .title {
+  font-family: var(--brand-font);
+  }
+`}
+</style>;
 
 export default App;
